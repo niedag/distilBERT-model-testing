8ad6a0075cd7990f8dc37a6c37c9ab17b6919490
niedag <grantnieda@gmail.com>
2024-04-09 12:29:22 -0700

    michells model works now

--- distilBERT_sentiment_michellejieli.py
+++ distilBERT_sentiment_michellejieli.py
@@ -2,8 +2,7 @@ from transformers import pipeline, AutoModelForSequenceClassification
 
 
 classifier = pipeline("sentiment-analysis", model="michellejieli/inappropriate_text_classifier")
-model = AutoModelForSequenceClassification.from_pretrained("michellejieli/inappropriate_text_classifier", from_pt=True)
-#print(classifier("I see you’ve set aside this special time to humiliate yourself in public."))
+model = AutoModelForSequenceClassification.from_pretrained("michellejieli/inappropriate_text_classifier")
 
 # Initial problem with loading this script due to lack of tf_model.h5 file which stores weights from TensorFlow
-print(model("I see you’ve set aside this special time to humiliate yourself in public."))
+print(classifier("I see you’ve set aside this special time to humiliate yourself in public."))
