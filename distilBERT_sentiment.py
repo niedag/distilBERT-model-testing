c6fafe5284ee7910ec0095a33ddfbef4b3221f14
niedag <grantnieda@gmail.com>
2024-04-11 21:38:28 -0700

    comments

--- distilBERT_sentiment.py
+++ distilBERT_sentiment.py
@@ -9,3 +9,4 @@ data = [
 print(basic_sentiment_pipline(data))
 
 # Model: 3 https://huggingface.co/distilbert/distilbert-base-uncased-finetuned-sst-2-english
+# This file runs the base distilbert model 
