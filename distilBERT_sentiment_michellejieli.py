76243f72767755cfbe51fc479c3ec02e77f7dde3
Grant Nieda <grantnieda@gmail.com>
2024-04-12 17:27:13 -0700

    stash

--- distilBERT_sentiment_michellejieli.py
+++ distilBERT_sentiment_michellejieli.py
@@ -5,10 +5,5 @@ classifier = pipeline("sentiment-analysis", model="michellejieli/inappropriate_t
 model = AutoModelForSequenceClassification.from_pretrained("michellejieli/inappropriate_text_classifier")
 
 # Initial problem with loading this script due to lack of tf_model.h5 file which stores weights from TensorFlow
-<<<<<<< Updated upstream
-print(classifier("Who handles the data at rest"))
-
-=======
 print(model("I see you’ve set aside this special time to humiliate yourself in public."))
 print(model("I feel so good about the changes I'm making!"))
->>>>>>> Stashed changes
