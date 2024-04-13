966b6b3767ef2f5ca1e540d0f21eea2b3fd8ee3a
Grant Nieda <grantnieda@gmail.com>
2024-04-12 18:01:04 -0700

    no more bs

--- distilBERT_sentiment_michellejieli.py
+++ distilBERT_sentiment_michellejieli.py
@@ -1,6 +1,5 @@
 from transformers import pipeline, AutoModelForSequenceClassification
 
-#model = AutoModelForSequenceClassification.from_pretrained("michellejieli/inappropriate_text_classifier")
 classifier = pipeline("sentiment-analysis", model="michellejieli/inappropriate_text_classifier")
 
 # Initial problem with loading this script due to lack of tf_model.h5 file which stores weights from TensorFlow
